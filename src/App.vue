693f54939477b8eb399648ed2c44d772f0cb37be
yuladp1 <yuladp1@gmail.com>
2022-11-07 23:24:49 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <inputEmail />
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import inputEmail from "./components/input-email.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { inputEmail },
 };
 </script>
 
@@ -22,5 +17,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eeee;
 }
 </style>
---
+++ src/components/input-email.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div> 
+    <form>
+        <label>Input email here:</label>
+      <input type="email" required />
+    </form>
+  </div>
+</template> 
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+  color: #aaaa;
+  display: inline-block;
+}
+label {
+  color: #aaaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+}
+</style>
