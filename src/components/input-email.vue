f1cabe222be97e0ba10761a02f81fe3fea1b9265
yuladp1 <yuladp1@gmail.com>
2022-11-07 23:54:33 +0100

    init commit

--- src/components/input-email.vue
+++ src/components/input-email.vue
@@ -3,10 +3,40 @@
   <div> 
     <form>
         <label>Input email here:</label>
-      <input type="email" required />
+        <input type="email" required v-model="email"/>
+        <label >Enter password:</label>
+        <input type="password" v-model="password">
+        <label>Role</label>
+        <select v-model="role" >
+            <option value="developer">Web developer</option>
+            <option value="desiner">Designer</option>
+            <option value="mentor">Mentor</option>
+        </select>
+        <div class="terms">
+            <input type="checkbox" required v-model="accept"/>
+            <label>Accept all terms</label>
+        </div>
     </form>
+    <div>
+        <p>Email: {{email}}</p>
+        <p>Password: {{password}}</p>
+        <p>Role: {{role}}</p>
+        <p>Accepting terms: {{accept}}</p>
+    </div>
   </div>
 </template> 
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+      accept: false,
+    };
+  },
+};
+</script>
 <style>
 form {
   max-width: 420px;
@@ -27,11 +57,25 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
   box-sizing: border-box;
   border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type="chekbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.terms {
+  display: flex;
+  flex-wrap: nowrap;
 }
 </style>
